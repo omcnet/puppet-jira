68fb8c7f564cc20afe2bfb8058f6bf9bef2e19a7
omc10 <technik@omc.net>
2015-06-23 15:17:20 +0200

    Add support for fedora 21

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,6 +8,8 @@ class jira::params {
 
   case $::osfamily {
     /RedHat/: {
+      # TODO: this should really be a fact telling us whether
+      # we use systemd or not
       if $::operatingsystemmajrelease == '7' {
         $json_packages           = 'rubygem-json'
         $service_file_location   = '/usr/lib/systemd/system/jira.service'
@@ -18,6 +20,14 @@ class jira::params {
         $service_file_location   = '/etc/init.d/jira'
         $service_file_template   = 'jira/jira.initscript.erb'
         $service_lockfile        = '/var/lock/subsys/jira'
+      } elsif $::operatingsystem == "Fedora" {
+        #TODO: make this a real comparison by number
+        if $::operatingsystemmajrelease == "21" {
+          $json_packages           = 'rubygem-json'
+          $service_file_location   = '/usr/lib/systemd/system/jira.service'
+          $service_file_template   = 'jira/jira.service.erb'
+          $service_lockfile        = '/var/lock/subsys/jira' 
+        }
       }
     } /Debian/: {
         $json_packages           = [ 'rubygem-json', 'ruby-json' ]
